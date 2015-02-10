c8c40e9dc6cd18c8def668611b5f349f310f5c07
Christoph Schöpf <ch.schoepf1@hotmail.com>
2015-02-10 03:40:18 +0100

    try fix2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   protect_from_forgery with: :null_session,
-      if: Proc.new { |c| c.request.format =~ %r{application/json} }
+      #if: Proc.new { |c| c.request.format =~ %r{application/json} }
   
   helper_method :current_user
   helper_method :logged_in
